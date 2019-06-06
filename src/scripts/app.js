9ed9de046edeee5a86f38319539a42ed321d5ca1
jhgriffin <john069s.griffin@libertymutual.com>
2019-06-06 19:15:08 -0400

    add nav

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PollContainer from '../containers/PollContainer';
+import {BrowserRouter} from 'react-router-dom';
+import App from '../containers/App';
 
-ReactDOM.render(
-    <PollContainer></PollContainer>, document.getElementById('app')
+ReactDOM.render((
+    //<PollContainer></PollContainer>, document.getElementById('app')
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>),
+    document.getElementById('app')
 );

badf52824571be157c151de26a5ea7e2e93205fd
jhgriffin <john069s.griffin@libertymutual.com>
2019-06-06 17:22:25 -0400

    TDD setup

--- src/components/__tests__/PollContainerSpec.js
+++ src/components/__tests__/PollContainerSpec.js
@@ -1,11 +1,50 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import PollContainer from '../../containers/PollContainer';
-
+import $ from 'jquery';
+import { callbackify } from 'util';
 
 describe('PollContainer', () => {
+    let wrapper;
+    beforeEach(()=>{
+        $.get = jest.fn((url, callback) => {
+        let response = {
+            poll:
+                {
+                    header: "Welcome to the Poll!",
+                    questions : [{
+                        question: "What is  best?",
+                        choices: [
+                            {value: "Tacos", label: "Tacos"},
+                            {value: "Pizza", label: "Pizza"},
+                            {value: "Cheese", label: "Cheese"}
+                        ],
+                        correctAnswer: "Pizza"
+                    },
+                    {
+                        question: "What's your favorite color?:",
+                        choices: [
+                            {value: "Orange", label: "Orange"},
+                            {value: "Blue", label: "Blue"}
+                        ],
+                        correctAnswer: "Blue"
+                        }
+                    ]
+                }
+            }
+            callback(response);
+        });
+        wrapper = shallow(<PollContainer />);
+    });
+
     it('renders witout problems', () => {
-        const pollContainer = shallow(<PollContainer />);
-        expect(pollContainer).toEqual(jasmine.anything());
+        expect(wrapper).toEqual(jasmine.anything());
+       
+    });
+
+    it('renders multiple questions if present', () => {
+        let numberOfQuestions = wrapper.find('PollQuestion');
+        expect(numberOfQuestions.length).toEqual(2);
+        
     });
 });
